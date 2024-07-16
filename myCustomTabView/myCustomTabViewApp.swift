cb83dc8e53133d7b93165ee7a055dbb9ba706719
Jerome <jmuzet@gmail.com>
2024-07-16 11:53:20 +0200

    Initial Commit

---
+++ myCustomTabView/myCustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCustomTabViewApp.swift
+//  myCustomTabView
+//
+//  Created by Jerome on 16/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct myCustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
